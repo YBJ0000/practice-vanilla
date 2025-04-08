5ea3a6db6f6bd5fd18a8dbb37d15495f7bf4a966
YBJ0000 <95595902+YBJ0000@users.noreply.github.com>
2025-04-08 21:39:05 +1000

    add an image changer

--- scripts/main.js
+++ scripts/main.js
@@ -1,2 +1,10 @@
-const myHeading = document.querySelector('h1');
-myHeading.textContent = 'Hello world!';
+const myImage = document.querySelector("img");
+
+myImage.addEventListener("click", () => {
+  const mySrc = myImage.getAttribute("src")
+  if (mySrc === "images/firefox-icon.png") {
+    myImage.setAttribute("src", "images/firefox-2.png");
+  } else {
+    myImage.setAttribute("src", "images/firefox-icon.png");
+  }
+})
